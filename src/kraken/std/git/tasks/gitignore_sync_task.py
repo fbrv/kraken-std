c646152b19d9fd538bf56f4db7b97870232be279
Luke Baxter <luke.bax@talktalk.net>
2023-03-16 14:38:54 +0000

    Comprehensive Global Gitignore File (non-fork) (#115)
    * In-code todos for cleanup and apply
    
    * Test push to fork
    
    * todo.md
    
    * Add empty check task
    
    * Implement default tokens, fetching and validating
    
    * implement render for GitignoreFile
    
    * Store backup of gitignore in .old
    
    * Make generated_content consistent between writes and reads
    
    * Implement check task
    
    * Fix hash mismatch between saving to disk and reading
    
    * Tidy up
    
    * Added consts and rename parse
    
    * Fix \r weirdness
    
    * Fix hash mismatch after IDEs strip \r
    
    * Add sorting to check task
    
    * Sync inherits from Task
    
    * Add exceptions and error handling
    
    * Remove todo.md
    
    * adds extra_paths parameter to gitignore task
    
    * Add docs and remove hs specific tokens
    
    * Add tests, fix linting and add changelog
    
    * Updated PR references in 1 changelogs.
    
    * Fix string concatenation
    
    Co-authored-by: Florian Eich <florian.eich@gmail.com>
    
    * Fix error message
    
    Co-authored-by: Florian Eich <florian.eich@gmail.com>
    
    * Remove extra paths as a configurable option
    
    * Fix up tests
    
    ---------
    
    Co-authored-by: David Buezas <david.buezas@helsing.ai>
    Co-authored-by: GitHub Action <github-action@users.noreply.github.com>
    Co-authored-by: Florian Eich <florian.eich@gmail.com>

--- src/kraken/std/git/tasks/gitignore_sync_task.py
+++ src/kraken/std/git/tasks/gitignore_sync_task.py
@@ -1,75 +1,59 @@
 from __future__ import annotations
 
+import logging
 from pathlib import Path
-from typing import Dict, List, Optional, Sequence
+from typing import Sequence
 
-from kraken.core.api import Project, Property
-from kraken.core.lib.render_file_task import RenderFileTask
+from kraken.core.api import Property, Task, TaskStatus
+from kraken.core.lib.check_file_contents_task import as_bytes
 
-from ..gitignore import GitignoreFile, parse_gitignore, sort_gitignore
+from ..gitignore import GitignoreException, GitignoreFile
+from .const import DEFAULT_KRAKEN_GITIGNORE_PATHS
+
+logger = logging.getLogger(__name__)
 
 
-class GitignoreSyncTask(RenderFileTask):
-    """This task ensures that a given set of entries are present in a `.gitignore` file.
+class GitignoreSyncTask(Task):
+    """This task ensures that a given set of entries are present in a `.gitignore` file. These entries
+    are made up of:
+        a) :attr`tokens`: Used to source paths from gitignore.io
+        b) :attr:`extra_paths`: Additional required paths
+        c) Custom paths added manually by the user directly to the .gitignore
 
-    The :attr:`header` property can be set to place the paths below a particular comment in the `.gitignore` file. If
-    there is no comment with the given text, it and the paths will be appended to the end of the file. When no header
-    is specified, only missing paths will be added to beginning of the `.gitignore` file.
-
-    If :attr:`sort` is enabled, the `.gitignore` file will be sorted (keeping paths grouped under their comments).
+    If :attr:`sort_paths` is enabled, the `.gitignore` file will be sorted (keeping paths grouped under their comments).
 
     It's common to group this task under the default `fmt` group, as it is similar to formatting a `.gitignore` file.
     """
 
-    file: Property[Path]
     sort_paths: Property[bool] = Property.config(default=True)
     sort_groups: Property[bool] = Property.config(default=False)
+    tokens: Property[Sequence[str]]
+    kraken_paths: Sequence[str] = DEFAULT_KRAKEN_GITIGNORE_PATHS
 
-    _paths: Dict[Optional[str], List[str]]
-
-    def __init__(self, name: str, project: Project) -> None:
-        super().__init__(name, project)
-        self._paths = {}
-        self.file.setcallable(lambda: self.project.directory / ".gitignore")
-        self.content.setcallable(lambda: self.get_file_contents(self.file.get()))
-
-    def add_paths(self, header: str | None, paths: Sequence[str]) -> None:
-        the_paths = self._paths.setdefault(header, [])
-        the_paths[:] = set(list(the_paths) + list(paths))
-
-    def get_file_contents(self, file: Path) -> str | bytes:
+    def generate_file_contents(self, file: Path) -> str | bytes:
+        gitignore = GitignoreFile([])
         if file.exists():
-            gitignore = parse_gitignore(file)
-        else:
-            gitignore = GitignoreFile([])
-
-        for comment_header, paths in self._paths.items():
-            has_paths = set(gitignore.paths())
-
-            if comment_header is not None:
-                # Remove all existing paths, we'll make sure they're located under the header.
-                for path in paths:
-                    if path in has_paths:
-                        gitignore.remove_path(path)
-                        has_paths.discard(path)
-
-                # Find the location of the header.
-                insert_index = gitignore.find_comment(comment_header)
-                if insert_index is None:
-                    gitignore.add_blank()
-                    gitignore.add_comment(comment_header)
-                    insert_index = len(gitignore.entries)
-                else:
-                    insert_index += 1
-
-            else:
-                insert_index = 0
-
-            for path in paths:
-                if path not in has_paths:
-                    gitignore.add_path(path, insert_index)
-                    insert_index += 1
-
-        gitignore = sort_gitignore(gitignore, self.sort_paths.get(), self.sort_groups.get())
-
+            try:
+                gitignore = GitignoreFile.parse(file)
+            except (GitignoreException, ValueError):
+                logger.warn("Malformed gitignore detected - reseting (previous version saved to .gitignore.old)")
+        gitignore.refresh_generated_content(tokens=self.tokens.get(), extra_paths=self.kraken_paths)
+        gitignore.refresh_generated_content_hash()
+        gitignore.sort_gitignore(self.sort_paths.get(), self.sort_groups.get())
         return gitignore.render()
+
+    def execute(self) -> TaskStatus:
+        file = self.project.directory / ".gitignore"
+        try:
+            content = self.generate_file_contents(file)
+            new_str = as_bytes(content, "utf-8")
+            if file.exists():
+                old_str = file.read_bytes()
+                if old_str != new_str:
+                    backup_file = self.project.directory / ".gitignore.old"
+                    backup_file.write_bytes(old_str)
+            file.write_bytes(new_str)
+            return TaskStatus.up_to_date('".gitignore" is up to date')
+
+        except GitignoreException as gitignore_exception:
+            return TaskStatus.failed(f"Could not generate to the gitignore file: {gitignore_exception}")
